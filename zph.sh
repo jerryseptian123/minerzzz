2c7d2330bf84f1f13370a01282c849ff7d89842b
jerryseptian123 <112109192+jerryseptian123@users.noreply.github.com>
2024-05-21 20:23:43 +0700

    Update zph.sh

--- zph.sh
+++ zph.sh
@@ -1,5 +1,4 @@
-sudo apt update
+
 wget https://github.com/Bendr0id/xmrigCC/releases/download/3.3.3/xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz && tar -xvf xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz && cd miner && mv xmrigDaemon ~/ && mv xmrigMiner ~/
-cd && clear
 ./xmrigDaemon -o 103.87.77.74:443:443 -u ZEPHs7219x3KgWW38QpLMoKc1JQdCcrjU48U2V8wwmNhbmcCm1kDVdT3EL21RGwNAd5zTGv5HA6X8BSL8qzFyvmLT4AeuqWyWRP -p DTTECH$(echo $(shuf -i 1-9999 -n 1)) -a rx/0 -k --donate-level 1
 
